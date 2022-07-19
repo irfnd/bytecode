b82881583d5efb3d78842372edd29c4fa216ba8f
Irfnd <irfandiabimanyu@gmail.com>
2022-07-19 14:05:59 +0700

    Update - FIx conflict

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const { SERVER_HOST, SERVER_PORT } = process.env;
-const errorControll = require('../server/controllers/error')
+const errorControll = require("../server/controllers/error");
 
 const express = require("express");
 const cors = require("cors");
@@ -10,25 +10,18 @@ const helmet = require("helmet");
 const app = express();
 const port = SERVER_PORT || 8000;
 
-<<<<<<< HEAD
-
-
-=======
 const db = require("./models");
 const syncdb = false;
 
 app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
->>>>>>> 9c18bb65d3baa2440a6f557909c2d2149c0856b3
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 require("./routes")(app);
-
-
-app.use(errorControll.error)
-app.use(errorControll.notFound)
+app.use(errorControll.error);
+app.use(errorControll.notFound);
 
 app.listen(port, () => {
   console.log(`> Server running on http://${SERVER_HOST}:${port}`);
