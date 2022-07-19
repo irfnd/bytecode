155ff07f0137d3b26124ad27832bede0c5a32a62
Irfnd <irfandiabimanyu@gmail.com>
2022-07-19 09:38:24 +0700

    Update - Initialize express index

---
+++ server/index.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const { SERVER_HOST, SERVER_PORT } = process.env;
+
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const app = express();
+const port = SERVER_PORT || 8000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(cors());
+app.use(morgan("dev"));
+
+app.listen(port, () => {
+  console.log(`> Server running on http://${SERVER_HOST}:${port}`);
+});
