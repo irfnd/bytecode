49546688e0c46d9223163803f56e93b08e19fa4e
Ikhsanazis <mxnzis@gmail.com>
2022-07-22 04:53:51 +0700

    update

--- client/src/components/organisms/Comments.js
+++ client/src/components/organisms/Comments.js
@@ -13,7 +13,7 @@ function Comments() {
 							<Card.Img variant="top" src={profil} className="roundedCircle" height={200} />
 							<Card.Body >
 								<Card.Title className="mt-3 text-center">Nama</Card.Title>
-                                <Card.Subtitle className="mb-2 text-muted text-center">Card Subtitle</Card.Subtitle>
+                                <Card.Subtitle className="mb-2 text-muted text-center">location</Card.Subtitle>
 								<Card.Text>
 									This is a longer card with supporting text below as a natural lead-in to additional content. This
 									content is a little bit longer.
