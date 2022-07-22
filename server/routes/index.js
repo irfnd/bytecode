f1ce7534d5c46835449bfb750c6358b12f23c696
Irfnd <irfandiabimanyu@gmail.com>
2022-07-22 13:27:42 +0700

    Update - Added jobseeker search & pagination

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,12 @@
 module.exports = (app) => {
-  app.use("/users", require("./usersRoute"));
+  // Users, Recuiters, Companies Routes
   app.use("/auth", require("./authRoute"));
   app.use("/profile", require("./profileRoute"));
 
+  app.use("/jobseekers", require("./jobseekerRoute"));
+
+  app.use("/users", require("./usersRoute"));
   app.use("/skills", require("./skillsRoute"));
   app.use("/work-exp", require("./workExpRoute"));
   app.use("/portfolio", require("./portfolioRoute"));
-
-  app.use("/search", require("./pagination"));
 };
--- server/routes/pagination.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-const search = require("../controllers/search/pagination");
-
-router.route("/").get(search.getData);
-
-module.exports = router;
