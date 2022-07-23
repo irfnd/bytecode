eb200f4db8a2137489ff97921b2eb2a719f0fc48
irfnd <irfandiabimanyu@gmail.com>
2022-07-23 13:52:01 +0700

    Update - Added joi-password

--- server/models/usersModel.js
+++ server/models/usersModel.js
@@ -1,5 +1,7 @@
 const { sequelizeJoi, Joi: joi } = require("sequelize-joi");
+const { joiPasswordExtendCore } = require("joi-password");
 const Joi = joi.extend(require("joi-phone-number"));
+const JoiPassword = Joi.extend(joiPasswordExtendCore);
 
 const users = (sequelize, Sequelize) => {
   sequelizeJoi(sequelize);
@@ -30,7 +32,14 @@ const users = (sequelize, Sequelize) => {
     password: {
       type: Sequelize.TEXT,
       allowNull: false,
-      schema: Joi.string().trim().required(),
+      schema: JoiPassword.string()
+        .min(8)
+        .minOfSpecialCharacters(1)
+        .minOfLowercase(1)
+        .minOfUppercase(1)
+        .minOfNumeric(1)
+        .noWhiteSpaces()
+        .required(),
     },
   });
 
