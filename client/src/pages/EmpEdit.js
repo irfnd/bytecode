7c1532d6cbb8251c10a7510aded24506150fe019
pitlimitCode <prabow14@gmail.com>
2022-07-24 11:36:43 +0700

    [EDIT] employee & company background fit to pages

--- client/src/pages/EmpEdit.js
+++ client/src/pages/EmpEdit.js
@@ -7,235 +7,237 @@ import loc from "../assets/icons/locLogo.png";
 import profil from "../assets/img/profil.jpg";
 
 import Navbar1 from "../components/organisms/Navbar1Adi";
-import Footer from "../components/organisms/Footer";
+import Footer from "../components/organisms/FooterAdi";
 
 export default function EmpEdit() {
 	return (
 		<>
 		<Navbar1/>
-		<Container className="gradientBGPage">
-			<Row className="py-5">
-				<Col sm={1} />
+		<div className="gradientBGPage">
+			<Container>
+				<Row className="py-5">
+					<Col sm={1} />
 
-				{/* LEFT SIDE BAR */}
-				<Col sm={3}>
-					{/* PROFILE */}
-					<Row>
-						<div className="whiteBg py-4 px-4">
-							<Row>
-								<Col />
-								<Col xs={6}>
-									<Image className="circleImage pb-3" src={profil} />
-									<Row className="pb-4">
-										<Link to="/#editAvatarCompanyURL" className="logoEdit inlineIconText">
-											<Image src={edit} /> 
-											<span>Edit</span>
-										</Link>
-									</Row>
-								</Col>
-								<Col />
-							</Row>
-							<div className="h1SideBarProfile pb-1">Louis Tomlinson</div>
-							<div className="h2SideBarProfile pb-2">Web Developer</div>
-
-							<Row className="pSideBarProfile pb-2">
-								<Image className="loc" src={loc} />
-								Purwokerto, Jawa Tengah
-							</Row>
-
-							<div className="pSideBarProfile">Freelancer</div>
-						</div>
-					</Row>
-
-					{/* BUTTON FOR SAVE & CANCEL OF EDITING */}
-					<div className="profilSaveCancel">
+					{/* LEFT SIDE BAR */}
+					<Col sm={3}>
+						{/* PROFILE */}
 						<Row>
-						<Link to="/em">
-							<Button className="Button doit mt-3 mb-2">Save</Button>
-						</Link>
-						<Link to="/em">
-								<Button className="Button cancel mb-2">Cancel</Button>
-						</Link>
+							<div className="whiteBg py-4 px-4">
+								<Row>
+									<Col />
+									<Col xs={6}>
+										<Image className="circleImage pb-3" src={profil} />
+										<Row className="pb-4">
+											<Link to="/#editAvatarCompanyURL" className="logoEdit inlineIconText">
+												<Image src={edit} /> 
+												<span>Edit</span>
+											</Link>
+										</Row>
+									</Col>
+									<Col />
+								</Row>
+								<div className="h1SideBarProfile pb-1">Louis Tomlinson</div>
+								<div className="h2SideBarProfile pb-2">Web Developer</div>
+
+								<Row className="pSideBarProfile pb-2">
+									<Image className="loc" src={loc} />
+									Purwokerto, Jawa Tengah
+								</Row>
+
+								<div className="pSideBarProfile">Freelancer</div>
+							</div>
 						</Row>
-					</div>
-				</Col>
 
-				{/* RIGHT SIDE BAR */}
-				<Col sm={7}>
-					{/* JOBSEEKER EDIT PROFILE FORM 1 */}
-					<div className="whiteBg ms-3 mb-3 pb-4">
-						<Container className="ps-4 pe-4">
-							<div className="titleEditProfile pt-4">Profile</div>
-						</Container>
+						{/* BUTTON FOR SAVE & CANCEL OF EDITING */}
+						<div className="profilSaveCancel">
+							<Row>
+							<Link to="/em">
+								<Button className="Button doit mt-3 mb-2">Save</Button>
+							</Link>
+							<Link to="/em">
+									<Button className="Button cancel mb-2">Cancel</Button>
+							</Link>
+							</Row>
+						</div>
+					</Col>
+
+					{/* RIGHT SIDE BAR */}
+					<Col sm={7}>
+						{/* JOBSEEKER EDIT PROFILE FORM 1 */}
+						<div className="whiteBg ms-3 mb-3 pb-4">
+							<Container className="ps-4 pe-4">
+								<div className="titleEditProfile pt-4">Profile</div>
+							</Container>
+							<hr />
+							<Container className="ps-4 pe-4">
+								<Form>
+									<Form.Group className="mb-4 pSideBarProfile" controlId="fullname">
+										<Form.Label className="mb-1">Full name</Form.Label>
+										<Form.Control size="md" type="email" placeholder="Enter full name" />
+										{/* <Form.Text className="text-muted">
+												We'll never share your email with anyone else.
+											</Form.Text> */}
+									</Form.Group>
+
+									<Form.Group className="mb-4 pSideBarProfile" controlId="jobdesk">
+										<Form.Label className="mb-1">Job desk</Form.Label>
+										<Form.Control size="md" type="text" placeholder="Enter job desk" />
+									</Form.Group>
+
+									<Form.Group className="mb-4 pSideBarProfile" controlId="domicile">
+										<Form.Label className="mb-1">Domicile</Form.Label>
+										<Form.Control size="md" type="text" placeholder="Enter domicile" />
+									</Form.Group>
+
+									<Form.Group className="mb-4 pSideBarProfile" controlId="workplace">
+										<Form.Label className="mb-1">Work Place</Form.Label>
+										<Form.Control size="md" type="text" placeholder="Work place" />
+									</Form.Group>
+
+									<Form.Group className="mb-4 pSideBarProfile" controlId="description">
+										<Form.Label className="mb-1">Description</Form.Label>
+										<Form.Control as="textarea" rows={5} size="md" placeholder="Enter description" />
+									</Form.Group>
+								</Form>
+							</Container>
+						</div>
+
+						{/* JOBSEEKER EDIT PROFILE FORM 2 */}
+						<div className="whiteBg ms-3 mb-3 pb-4">
+							<Container className="ps-4 pe-4">
+								<div className="titleEditProfile pt-4">Skills</div>
+							</Container>
+							<hr />
+							<Container className="ps-4 pe-4">
+								<Form>
+									<Row>
+										<Col sm={10}>
+											<Form.Group className="mb-3 pSideBarProfile" controlId="skill">
+												<Form.Control size="lg" type="text" placeholder="Enter skill" />
+											</Form.Group>
+										</Col>
+										<Col sm={2}>
+											<Button className="Button skillbutton" variant="primary" type="submit">
+												Save
+											</Button>
+										</Col>
+									</Row>
+								</Form>
+							</Container>
+						</div>
+
+						{/* JOBSEEKER EDIT PROFILE FORM 3 */}
+						<div className="whiteBg ms-3 mb-3 pb-4">
+							<Container className="ps-4 pe-4">
+								<div className="titleEditProfile pt-4">Job Experiences</div>
+							</Container>
+							<hr />
+							<Container className="ps-4 pe-4">
+								<Form>
+									<Form.Group className="mb-4 pSideBarProfile" controlId="position">
+										<Form.Label className="mb-1">Position</Form.Label>
+										<Form.Control size="md" type="text" placeholder="Web developer, Mobile app developer" />
+									</Form.Group>
+
+									<Row>
+										<Col>
+											<Form.Group className="mb-4 pSideBarProfile" controlId="company">
+												<Form.Label className="mb-1">Company name</Form.Label>
+												<Form.Control size="md" type="text" placeholder="PT. Company Corp" />
+											</Form.Group>
+										</Col>
+										<Col>
+											<Form.Group className="mb-4 pSideBarProfile" controlId="domicile">
+												<Form.Label className="mb-1">Month/Year</Form.Label>
+												<Form.Control size="md" type="text" placeholder="July 2022" />
+											</Form.Group>
+										</Col>
+									</Row>
+
+									<Form.Group className="mb-4 pSideBarProfile" controlId="workplace">
+										<Form.Label className="mb-1">Description</Form.Label>
+										<Form.Control as="textarea" rows={4} size="md" placeholder="Description your jobs" />
+									</Form.Group>
+
+									<Button
+										className="Button skillbutton"
+										variant="primary"
+										// type="submit"
+										// onClick={()}
+									>
+										Add Job Experiences
+									</Button>
+								</Form>
+							</Container>
+						</div>
+
+						{/* JOBSEEKER EDIT PROFILE FORM 4 */}
+						<div className="whiteBg ms-3 mb-3 pb-4">
+							<Container className="ps-4 pe-4">
+								<div className="titleEditProfile pt-4">Portofolio</div>
+							</Container>
+							<hr />
+							<Container className="ps-4 pe-4">
+								<Form>
+									<Form.Group className="mb-3 pSideBarProfile" controlId="appsname">
+										<Form.Label>Apps name</Form.Label>
+										<Form.Control size="md" type="text" placeholder="Enter apps" />
+									</Form.Group>
+
+									<Form.Group className="mb-3 pSideBarProfile" controlId="linkrepository">
+										<Form.Label>Link repository</Form.Label>
+										<Form.Control size="md" type="text" placeholder="Enter link repository" />
+									</Form.Group>
+
+									<Form.Group className="mb-3 pSideBarProfile" controlId="domicile">
+										{["radio"].map((type) => (
+											<div key={`inline-${type}`} className="mb-3">
+												<Form.Check inline label="Mobile Apps" name="group1" type={type} id={`inline-${type}-1`} />
+												<Form.Check inline label="Mobile Apps" name="group1" type={type} id={`inline-${type}-2`} />
+											</div>
+										))}
+									</Form.Group>
+
+									<Form.Group className="mb-3 pSideBarProfile" controlId="fileImage">
+										<Form.Label>Upload image</Form.Label>
+										<Form.Control size="md" type="file" />
+									</Form.Group>
+
+									<Button
+										className="Button skillbutton"
+										variant="primary"
+										// type="submit"
+										// onClick={()}
+									>
+										Add Portofolio
+									</Button>
+								</Form>
+							</Container>
+						</div>
+					</Col>
+
+					<Col sm={1} />
+				</Row>
+				
+				{/* <Row className="bg ">
+					<Col lg={10} className="mx-auto mt-5">
+						<Image src={logo2} height={50} className="mt-3 mb-3" />
+						<p className="mb-5">With supporting text below as a natural lead-in to additional content.</p>
 						<hr />
-						<Container className="ps-4 pe-4">
-							<Form>
-								<Form.Group className="mb-4 pSideBarProfile" controlId="fullname">
-									<Form.Label className="mb-1">Full name</Form.Label>
-									<Form.Control size="md" type="email" placeholder="Enter full name" />
-									{/* <Form.Text className="text-muted">
-											We'll never share your email with anyone else.
-										</Form.Text> */}
-								</Form.Group>
-
-								<Form.Group className="mb-4 pSideBarProfile" controlId="jobdesk">
-									<Form.Label className="mb-1">Job desk</Form.Label>
-									<Form.Control size="md" type="text" placeholder="Enter job desk" />
-								</Form.Group>
-
-								<Form.Group className="mb-4 pSideBarProfile" controlId="domicile">
-									<Form.Label className="mb-1">Domicile</Form.Label>
-									<Form.Control size="md" type="text" placeholder="Enter domicile" />
-								</Form.Group>
-
-								<Form.Group className="mb-4 pSideBarProfile" controlId="workplace">
-									<Form.Label className="mb-1">Work Place</Form.Label>
-									<Form.Control size="md" type="text" placeholder="Work place" />
-								</Form.Group>
-
-								<Form.Group className="mb-4 pSideBarProfile" controlId="description">
-									<Form.Label className="mb-1">Description</Form.Label>
-									<Form.Control as="textarea" rows={5} size="md" placeholder="Enter description" />
-								</Form.Group>
-							</Form>
-						</Container>
-					</div>
-
-					{/* JOBSEEKER EDIT PROFILE FORM 2 */}
-					<div className="whiteBg ms-3 mb-3 pb-4">
-						<Container className="ps-4 pe-4">
-							<div className="titleEditProfile pt-4">Skills</div>
-						</Container>
-						<hr />
-						<Container className="ps-4 pe-4">
-							<Form>
-								<Row>
-									<Col sm={10}>
-										<Form.Group className="mb-3 pSideBarProfile" controlId="skill">
-											<Form.Control size="lg" type="text" placeholder="Enter skill" />
-										</Form.Group>
-									</Col>
-									<Col sm={2}>
-										<Button className="Button skillbutton" variant="primary" type="submit">
-											Save
-										</Button>
-									</Col>
-								</Row>
-							</Form>
-						</Container>
-					</div>
-
-					{/* JOBSEEKER EDIT PROFILE FORM 3 */}
-					<div className="whiteBg ms-3 mb-3 pb-4">
-						<Container className="ps-4 pe-4">
-							<div className="titleEditProfile pt-4">Job Experiences</div>
-						</Container>
-						<hr />
-						<Container className="ps-4 pe-4">
-							<Form>
-								<Form.Group className="mb-4 pSideBarProfile" controlId="position">
-									<Form.Label className="mb-1">Position</Form.Label>
-									<Form.Control size="md" type="text" placeholder="Web developer, Mobile app developer" />
-								</Form.Group>
-
-								<Row>
-									<Col>
-										<Form.Group className="mb-4 pSideBarProfile" controlId="company">
-											<Form.Label className="mb-1">Company name</Form.Label>
-											<Form.Control size="md" type="text" placeholder="PT. Company Corp" />
-										</Form.Group>
-									</Col>
-									<Col>
-										<Form.Group className="mb-4 pSideBarProfile" controlId="domicile">
-											<Form.Label className="mb-1">Month/Year</Form.Label>
-											<Form.Control size="md" type="text" placeholder="July 2022" />
-										</Form.Group>
-									</Col>
-								</Row>
-
-								<Form.Group className="mb-4 pSideBarProfile" controlId="workplace">
-									<Form.Label className="mb-1">Description</Form.Label>
-									<Form.Control as="textarea" rows={4} size="md" placeholder="Description your jobs" />
-								</Form.Group>
-
-								<Button
-									className="Button skillbutton"
-									variant="primary"
-									// type="submit"
-									// onClick={()}
-								>
-									Add Job Experiences
-								</Button>
-							</Form>
-						</Container>
-					</div>
-
-					{/* JOBSEEKER EDIT PROFILE FORM 4 */}
-					<div className="whiteBg ms-3 mb-3 pb-4">
-						<Container className="ps-4 pe-4">
-							<div className="titleEditProfile pt-4">Portofolio</div>
-						</Container>
-						<hr />
-						<Container className="ps-4 pe-4">
-							<Form>
-								<Form.Group className="mb-3 pSideBarProfile" controlId="appsname">
-									<Form.Label>Apps name</Form.Label>
-									<Form.Control size="md" type="text" placeholder="Enter apps" />
-								</Form.Group>
-
-								<Form.Group className="mb-3 pSideBarProfile" controlId="linkrepository">
-									<Form.Label>Link repository</Form.Label>
-									<Form.Control size="md" type="text" placeholder="Enter link repository" />
-								</Form.Group>
-
-								<Form.Group className="mb-3 pSideBarProfile" controlId="domicile">
-									{["radio"].map((type) => (
-										<div key={`inline-${type}`} className="mb-3">
-											<Form.Check inline label="Mobile Apps" name="group1" type={type} id={`inline-${type}-1`} />
-											<Form.Check inline label="Mobile Apps" name="group1" type={type} id={`inline-${type}-2`} />
-										</div>
-									))}
-								</Form.Group>
-
-								<Form.Group className="mb-3 pSideBarProfile" controlId="fileImage">
-									<Form.Label>Upload image</Form.Label>
-									<Form.Control size="md" type="file" />
-								</Form.Group>
-
-								<Button
-									className="Button skillbutton"
-									variant="primary"
-									// type="submit"
-									// onClick={()}
-								>
-									Add Portofolio
-								</Button>
-							</Form>
-						</Container>
-					</div>
-				</Col>
-
-				<Col sm={1} />
-			</Row>
-			
-			{/* <Row className="bg ">
-				<Col lg={10} className="mx-auto mt-5">
-					<Image src={logo2} height={50} className="mt-3 mb-3" />
-					<p className="mb-5">With supporting text below as a natural lead-in to additional content.</p>
-					<hr />
-					<Row>
-						<Navbar>
-							<p>2020 Pewworld. All right reserved</p>
-							<Navbar.Collapse className="footerlink justify-content-end">
-								<p className="mx-5">Telepon</p>
-								<p>Email</p>
-							</Navbar.Collapse>
-						</Navbar>
-					</Row>
-				</Col>
-			</Row> */}
-			<Footer/>
-		</Container>
+						<Row>
+							<Navbar>
+								<p>2020 Pewworld. All right reserved</p>
+								<Navbar.Collapse className="footerlink justify-content-end">
+									<p className="mx-5">Telepon</p>
+									<p>Email</p>
+								</Navbar.Collapse>
+							</Navbar>
+						</Row>
+					</Col>
+				</Row> */}
+			</Container>
+		</div>
+		<Footer/>
 		</>
 	);
 }
--- client/src/pages/Hire.js
+++ client/src/pages/Hire.js
@@ -7,115 +7,117 @@ import profil from "../assets/img/profil.jpg";
 import loc from "../assets/icons/locLogo.png"; //
 
 import Navbar1 from "../components/organisms/Navbar1Adi";
-import Footer from "../components/organisms/Footer";
+import Footer from "../components/organisms/FooterAdi";
 
 export default function Hire() {
 	return (
 		<>
 		<Navbar1 />
-		<Container className="solidBGPage">
-			<Row className="py-5">
-				<Col sm={1} />
+		<div className="solidBGPage">
+			<Container>
+				<Row className="py-5">
+					<Col sm={1} />
 
-				{/* LEFT SIDE BAR */}
-				<Col sm={3} className="me-3">
-					<Row>
-						<div className="whiteBg pt-4 pb-2 px-4">
-							<Row>
-								<Col />
-								<Col xs={6}>
-									<Image className="circleImage pb-4" src={profil} />
-								</Col>
-								<Col />
-							</Row>
-							<div className="h1SideBarProfile pb-1">Louis Tomlinson</div>
+					{/* LEFT SIDE BAR */}
+					<Col sm={3} className="me-3">
+						<Row>
+							<div className="whiteBg pt-4 pb-2 px-4">
+								<Row>
+									<Col />
+									<Col xs={6}>
+										<Image className="circleImage pb-4" src={profil} />
+									</Col>
+									<Col />
+								</Row>
+								<div className="h1SideBarProfile pb-1">Louis Tomlinson</div>
 
-							<div className="deskCompJSeekProfile pb-3">
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+								<div className="deskCompJSeekProfile pb-3">
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+								</div>
+
+								<Row className="pSideBarProfile pb-3">
+									<Image className="loc" src={loc} />
+									Purwokerto, Jawa Tengah
+								</Row>
+
+								<div className="pSideBarProfile pb-1">
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+									dolore magna aliqua. Ut enim ad minim veniam, quis
+									{/* nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum */}
+								</div>
+
+								<div className="h1SideBarProfile mb-2">Skill</div>
+								<div className="mb-4 d-flex flex-wrap">
+									<div className="theTag">javascript</div>
+									<div className="theTag">python</div>
+									<div className="theTag">java</div>
+									<div className="theTag">C++</div>
+									<div className="theTag">php</div>
+									<div className="theTag">Golang</div>
+								</div>
 							</div>
+						</Row>
+					</Col>
 
-							<Row className="pSideBarProfile pb-3">
-								<Image className="loc" src={loc} />
-								Purwokerto, Jawa Tengah
-							</Row>
-
-							<div className="pSideBarProfile pb-1">
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-								dolore magna aliqua. Ut enim ad minim veniam, quis
-								{/* nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum */}
-							</div>
-
-							<div className="h1SideBarProfile mb-2">Skill</div>
-							<div className="mb-4 d-flex flex-wrap">
-								<div className="theTag">javascript</div>
-								<div className="theTag">python</div>
-								<div className="theTag">java</div>
-								<div className="theTag">C++</div>
-								<div className="theTag">php</div>
-								<div className="theTag">Golang</div>
-							</div>
+					{/* RIGHT SIDE BAR checkBGCompJSeekProfile */}
+					<Col sm={7} className="ps-5 pe-5">
+						<div className="titleContactJseek mb-2">Contact to NAME_JOBSEEKER</div>
+						<div className="deskCompJSeekProfile mb-5">
+							Description before hire Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+							incididunt ut
 						</div>
-					</Row>
-				</Col>
 
-				{/* RIGHT SIDE BAR checkBGCompJSeekProfile */}
-				<Col sm={7} className="ps-5 pe-5">
-					<div className="titleContactJseek mb-2">Contact to NAME_JOBSEEKER</div>
-					<div className="deskCompJSeekProfile mb-5">
-						Description before hire Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-						incididunt ut
-					</div>
+						{/* Form for Company to hiring Jobseeker */}
+						<Form>
+							<Form.Group className="mb-3 pSideBarProfile" controlId="purpose">
+								<Form.Label>Purpose of this message</Form.Label>
+								<Form.Select size="md" aria-label="Default select example" style={{ color: "gray" }}>
+									<option>Project Select</option>
+									<option style={{ color: "black" }} value="1">
+										ProjectOne
+									</option>
+									<option style={{ color: "black" }} value="2">
+										ProjectTwo
+									</option>
+									<option style={{ color: "black" }} value="3">
+										ProjectThree
+									</option>
+								</Form.Select>
+							</Form.Group>
 
-					{/* Form for Company to hiring Jobseeker */}
-					<Form>
-						<Form.Group className="mb-3 pSideBarProfile" controlId="purpose">
-							<Form.Label>Purpose of this message</Form.Label>
-							<Form.Select size="md" aria-label="Default select example" style={{ color: "gray" }}>
-								<option>Project Select</option>
-								<option style={{ color: "black" }} value="1">
-									ProjectOne
-								</option>
-								<option style={{ color: "black" }} value="2">
-									ProjectTwo
-								</option>
-								<option style={{ color: "black" }} value="3">
-									ProjectThree
-								</option>
-							</Form.Select>
-						</Form.Group>
+							<Form.Group className="mb-4 pSideBarProfile" controlId="fullname">
+								<Form.Label className="mb-1">Full name</Form.Label>
+								<Form.Control size="md" type="text" placeholder="Input full name" />
+								{/* <Form.Text className="text-muted">
+										We'll never share your email with anyone else.
+									</Form.Text> */}
+							</Form.Group>
 
-						<Form.Group className="mb-4 pSideBarProfile" controlId="fullname">
-							<Form.Label className="mb-1">Full name</Form.Label>
-							<Form.Control size="md" type="text" placeholder="Input full name" />
-							{/* <Form.Text className="text-muted">
-									We'll never share your email with anyone else.
-								</Form.Text> */}
-						</Form.Group>
+							<Form.Group className="mb-4 pSideBarProfile" controlId="email">
+								<Form.Label className="mb-1">email</Form.Label>
+								<Form.Control size="md" type="text" placeholder="Input email" />
+							</Form.Group>
 
-						<Form.Group className="mb-4 pSideBarProfile" controlId="email">
-							<Form.Label className="mb-1">email</Form.Label>
-							<Form.Control size="md" type="text" placeholder="Input email" />
-						</Form.Group>
+							<Form.Group className="mb-4 pSideBarProfile" controlId="phone">
+								<Form.Label className="mb-1">Handphone number</Form.Label>
+								<Form.Control size="md" type="number" placeholder="Input handphone number" />
+							</Form.Group>
 
-						<Form.Group className="mb-4 pSideBarProfile" controlId="phone">
-							<Form.Label className="mb-1">Handphone number</Form.Label>
-							<Form.Control size="md" type="number" placeholder="Input handphone number" />
-						</Form.Group>
+							<Form.Group className="mb-4 pSideBarProfile" controlId="description">
+								<Form.Label className="mb-1">Description</Form.Label>
+								<Form.Control as="textarea" rows={6} size="md" placeholder="Description" />
+							</Form.Group>
 
-						<Form.Group className="mb-4 pSideBarProfile" controlId="description">
-							<Form.Label className="mb-1">Description</Form.Label>
-							<Form.Control as="textarea" rows={6} size="md" placeholder="Description" />
-						</Form.Group>
+							<Button className="Button doit my-4">Hire</Button>
+						</Form>
+					</Col>
 
-						<Button className="Button doit my-4">Hire</Button>
-					</Form>
-				</Col>
-
-				<Col sm={1} />
-			</Row>
-			
-			<Footer />
-		</Container>
+					<Col sm={1} />
+				</Row>
+				
+			</Container>
+		</div>
+		<Footer />
 		</>
 	);
 }
