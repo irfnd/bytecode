31271af481e312992ac49c2ffeaadc3cd22a0284
Irfnd <irfandiabimanyu@gmail.com>
2022-07-24 21:45:19 +0700

    Update - Edited commented socket.io

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const { SERVER_HOST, SERVER_PORT, CLIENT_HOST, PORT } = process.env;
+const { SERVER_HOST, CLIENT_HOST, PORT } = process.env;
 
 const express = require("express");
 const cors = require("cors");
@@ -8,14 +8,13 @@ const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
 
 const app = express();
-const port = SERVER_PORT || 8000;
 const http = require("http");
 const server = http.createServer(app);
 const io = require("socket.io")(server, {
   cors: {
     origin: "*",
-    methods: ["GET", "POST"]
-  }
+    methods: ["GET", "POST"],
+  },
 });
 
 const db = require("./models");
@@ -33,42 +32,39 @@ require("./routes/index")(app);
 
 app.use(errorHandling);
 
+// app.get("/chat", (req, res) => {
+//   res.sendFile(__dirname + "/example.html");
+// });
 
+// io.on("connection", (socket) => {
+//   console.log("connected to", socket.id);
+//   const room = "MYROOM";
 
-app.get('/chat', (req, res) => {
-  res.sendFile(__dirname + '/example.html');
-});
+//   socket.on("adduser", (username) => {
+//     socket.user = username;
+//     users.push(username);
+//     console.log("latest users", users);
+//     io.sockets.emit("users", users);
+//   });
+//   socket.on("message", (message) => {
+//     io.sockets.emit("message", {
+//       user: socket.user,
+//       message: message,
+//     });
+//   });
+//   socket.on("disconnect", () => {
+//     console.log("deleting ", socket.user);
 
+//     if (socket.user) {
+//       users.splice(users.indexOf(socket.user), 1);
+//     }
+//     io.sockets.emit("users", users);
+//     console.log("remaining users: ", users);
+//   });
+// });
 
-io.on('connection', (socket) => {
-  console.log('connected to', socket.id)
-  const room = 'MYROOM'
-
-  socket.on("adduser", (username) => {
-      socket.user = username;
-      users.push(username)
-      console.log("latest users", users)
-      io.sockets.emit("users", users)
-  })
-  socket.on("message", (message) => {
-      io.sockets.emit("message", {
-          user: socket.user,
-          message: message,
-      })
-  })
-  socket.on("disconnect", () => {
-      console.log("deleting ", socket.user)
-
-      if (socket.user) {
-          users.splice(users.indexOf(socket.user), 1);
-      }
-      io.sockets.emit("users", users)
-      console.log('remaining users: ', users)
-  })
-});
-
-server.listen(port || 8000, () => {
-  console.log(`\n> Server running on http://${SERVER_HOST}:${port}`);
+server.listen(PORT || 8000, () => {
+  console.log(`\n> Server running on http://${SERVER_HOST}:${PORT}`);
   db.sequelize
     .sync({ force: syncdb })
     .then(() => console.log("> Connected to database\n"))
