e4030bb4ab27fc848d416e49f279e9a037bd76d4
Nanang Komarudin <45787278+nanangNSL@users.noreply.github.com>
2022-07-26 04:06:29 +0700

    [Update realtime chat]

--- server/index.js
+++ server/index.js
@@ -34,7 +34,20 @@ require("./routes/index")(app);
 app.use(errorHandling);
 
 io.on("connection", (socket) => {
-  socket.on('socket on by : ', socket.id)
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
 });
 
 server.listen(port, () => {
